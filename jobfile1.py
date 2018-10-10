fe6b7b966c7e4deb2927c0b43fd891ff0178f722
walterjrbr <waugustojr@gmail.com>
2018-10-10 18:37:05 -0300

    commit2

---
+++ jobfile1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import FloatType
+
+
+
+spark = SparkSession \
+        .builder \
+        .appName("Processa arquivo de dados") \
+        .getOrCreate()
+
+to_value = lambda v: float(v.replace(",","."))
+udf_to_value = F.udf(to_value, pyspark.sql.types.FloatType())
+
+df=spark.read.csv("/tmp/spark/201302_Diarias.utf8.csv",header=True,sep="\t")
+
+df2=df.withColumn("value", udf_to_value(df["Valor Pagamento"])) \
+    .withColumn("Dtpg", F.to_date(df["Data Pagamento"], format="dd/MM/yyyy"))
+
+df3=df2.select(df2["Nome Órgão Superior"].alias("Orgao"),df2["Data Pagamento"].alias("DtPg"))
+
+df3.show()
